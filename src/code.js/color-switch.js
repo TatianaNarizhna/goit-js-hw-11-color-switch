91d0c7f28d572f290a592ee666aef71d0fe313ed
TatianaNarizhna <narizhna.tania@gmail.com>
2021-05-19 21:27:15 +0300

    working

--- src/code.js/color-switch.js
+++ src/code.js/color-switch.js
@@ -6,3 +6,34 @@ const colors = [
     '#009688',
     '#795548',
   ];
+
+  const refs = {
+      start: document.querySelector('button[data-action="start"]'),
+      stop: document.querySelector('button[data-action="stop"]'),
+      bodyColor: document.querySelector('body'),
+  }
+
+  const TIME_DELAY = 1000;
+
+  refs.start.addEventListener('click', startColorSwitch);
+//   refs.stop.addEventListener('click', callback);
+
+  const randomIntegerFromInterval = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+function randomColor () {
+    const randomCol = colors[randomIntegerFromInterval(0, colors.length-1)];
+   console.log(randomCol);
+   
+    refs.bodyColor.style.backgroundColor = randomCol;
+    
+};
+
+  function startColorSwitch (e) {
+    
+    setInterval(() => {
+        randomColor();
+
+    }, TIME_DELAY);
+  }
